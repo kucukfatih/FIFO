2e05da732a0f4e8e7c8188b7c649e48226ebe605
kucukfatih <153717596+kucukfatih@users.noreply.github.com>
2024-04-19 13:08:01 +0300

    Add files via upload
    Gated clock olan okuma yazma lojiği değiştirilip, load enable ve read enable eklendi.

--- memory.v
+++ memory.v
@@ -28,21 +28,25 @@ module memory#(parameter width = 16, parameter depth = 8,parameter adr_width = $
     input wire [adr_width-1:0] r_adr,
     input wire load,
     input wire read,
+    input wire clk,
     output reg [width-1:0]data_out
 
     );
     
     reg [width-1:0] mem [depth-1:0];
     
-    always @(posedge load) begin
-    
+    always @(posedge clk,posedge load) begin
+        if(load)
             mem[w_adr] <= data_in;
-        
+        else
+            mem[w_adr] <= mem[w_adr];
     end
-     always @(posedge read) begin
-    
+     always @(posedge clk,posedge read) begin
+        if(read)
             data_out <= mem[r_adr];
-        
+        else
+            data_out <= data_out;
+
     end
     
 endmodule
